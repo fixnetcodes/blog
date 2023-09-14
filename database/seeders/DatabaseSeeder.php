cda0d034a942db744a6ebdbe6e3b1012ad8a682f
fixnetcodes <deogratiasamas@gmail.com>
2023-09-14 17:37:12 +0300

    created dummy data using database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $adminRole = Role::create(['name' => 'admin']);
+        $authorRole = Role::create(['name' => 'author']);
+        $userRole = Role::create(['name' => 'user']);
+
+        $manageUsersPermission = Permission::create(['name' => 'manage users']);
+        $createPostPermission = Permission::create(['name' => 'create post']);
+        $editOwnPostPermission = Permission::create(['name' => 'edit own post']);
+
+        $adminRole->givePermissionTo($manageUsersPermission);
+        $authorRole->givePermissionTo($createPostPermission);
+        $authorRole->givePermissionTo($editOwnPostPermission);
     }
 }
