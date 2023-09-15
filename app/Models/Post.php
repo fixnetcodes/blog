6a4e4474591ad893a1cebb85e6b9e7fc128c19f0
fixnetcodes <deogratiasamas@gmail.com>
2023-09-15 15:24:35 +0300

    created routes for posts creation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,7 @@ class Post extends Model
     protected $table = 'posts';
 
     protected $fillable = [
-        'title', 'description'
+        'user_id', 'title', 'description'
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -33,6 +34,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::group(["prefix"=>'posts'], function(){
+        Route::get('/new', [PostController::class, 'newPost'])->name('posts.add');
+        Route::post('/store', [PostController::class, 'postSubmit'])->name('posts.store');
+    });
 });
 
 require __DIR__.'/auth.php';
