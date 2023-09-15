56cb6617854b51afe6c0c70c8c49645a78f54be1
fixnetcodes <deogratiasamas@gmail.com>
2023-09-15 15:23:47 +0300

    added controller function to display posts form and submit post in the db

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+
+
+class PostController extends Controller
+{
+    public function newPost()
+    {
+        return Inertia::render('Post/AddNew');
+    }
+
+    public function postSubmit(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+        ]);
+        // dd($request->all());
+
+        try {
+            $post = Post::create([
+                'user_id' => auth()->id(),
+                'title' => $request->title,
+                'description' => $request->description,
+            ]);
+            return redirect()->back()->with('success', 'Post created successfully!');
+
+        }catch(\Exception $e){
+            return redirect()->back()->with('error', 'An error occurred while creating the post.');
+        }
+    }
+}
